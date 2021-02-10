3fc05c792bb22a88a42528b9abb1c95ed1f286b2
ikramelhattab <ikramelhattab@90@gmail.com>
2021-02-10 22:28:32 +0000

    Object Detection

---
+++ object_detection_webcam.py
@@ -0,0 +1,204 @@
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import pathlib
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+from IPython.display import display
+
+
+
+
+
+# In[ ]:
+
+
+from utils import ops as utils_ops
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+
+
+
+# In[ ]:
+
+
+
+utils_ops.tf = tf.compat.v1
+
+
+tf.gfile = tf.io.gfile
+
+
+
+# In[ ]:
+
+
+def load_model(model_name):
+  base_url = 'http://download.tensorflow.org/models/object_detection/'
+  model_file = model_name + '.tar.gz'
+  model_dir = tf.keras.utils.get_file(
+    fname=model_name, 
+    origin=base_url + model_file,
+    untar=True)
+
+  model_dir = pathlib.Path(model_dir)/"saved_model"
+
+  model = tf.saved_model.load(str(model_dir))
+  model = model.signatures['serving_default']
+
+  return model
+
+
+
+# In[ ]:
+
+
+PATH_TO_LABELS = 'data/mscoco_label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+
+
+
+# In[ ]:
+
+
+PATH_TO_TEST_IMAGES_DIR =  pathlib.Path('test_images')
+TEST_IMAGE_PATHS = sorted(list(PATH_TO_TEST_IMAGES_DIR.glob("*.jpg")))
+TEST_IMAGE_PATHS
+
+
+
+# In[ ]:
+
+
+model_name = 'ssd_mobilenet_v1_coco_2018_01_28'
+detection_model = load_model(model_name)
+
+
+# In[ ]:
+
+
+print(detection_model.inputs)
+
+
+# In[ ]:
+
+
+detection_model.output_dtypes
+
+
+# In[ ]:
+
+
+detection_model.output_shapes
+
+
+
+# In[ ]:
+
+
+def run_inference_for_single_image(model, image):
+  image = np.asarray(image)
+  input_tensor = tf.convert_to_tensor(image)
+  input_tensor = input_tensor[tf.newaxis,...]
+
+  output_dict = model(input_tensor)
+
+  
+  num_detections = int(output_dict.pop('num_detections'))
+  output_dict = {key:value[0, :num_detections].numpy() 
+                 for key,value in output_dict.items()}
+  output_dict['num_detections'] = num_detections
+
+  output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+   
+  if 'detection_masks' in output_dict:
+    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+              output_dict['detection_masks'], output_dict['detection_boxes'],
+               image.shape[0], image.shape[1])      
+    detection_masks_reframed = tf.cast(detection_masks_reframed > 0.5,
+                                       tf.uint8)
+    output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+    
+  return output_dict
+
+
+# In[ ]:
+#import sys
+#print(sys.path)
+import cv2
+
+#sys.path.append('\ProgramData\Anaconda3\Lib\site-packages')
+
+
+def show_inference(model, image_path):
+
+ cap = cv2.VideoCapture(0)
+
+ ret = True 
+ while (ret):
+  
+  #image_np = np.array(Image.open(image_path))
+  ret,image_np = cap.read()
+        # Actual detection.
+  output_dict = run_inference_for_single_image(model, image_np)
+  # Visualization of the results of a detection.
+  vis_util.visualize_boxes_and_labels_on_image_array(
+      image_np,
+      output_dict['detection_boxes'],
+      output_dict['detection_classes'],
+      output_dict['detection_scores'],
+      category_index,
+      instance_masks=output_dict.get('detection_masks_reframed', None),
+      use_normalized_coordinates=True,
+      line_thickness=8)
+  cv2.imshow('application detection objets',cv2.resize(image_np,(1280,960)))
+  if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+      cv2.destroyAllWindows()
+      cap.release()
+  #display(Image.fromarray(image_np))
+
+
+# In[ ]:
+
+
+for image_path in TEST_IMAGE_PATHS:
+  show_inference(detection_model, image_path)
+
+
+
+# In[ ]:
+
+
+model_name = "mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28"
+masking_model = load_model("mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28")
+
+
+
+# In[ ]:
+
+
+masking_model.output_shapes
+
+
+# In[ ]:
+
+
+for image_path in TEST_IMAGE_PATHS:
+  show_inference(masking_model, image_path)
+
+
+# In[ ]:
+
+
+
+
